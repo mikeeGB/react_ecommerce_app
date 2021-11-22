0ecb44d2192fdbb770c6bb2e68add4173793610d
mike <samkomisha@gmail.com>
2021-11-22 13:51:52 +0300

    add shop info route path

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Home} from "./components/Home";
 import {Route, BrowserRouter, Switch} from "react-router-dom";
+import ShopInformation from "./components/shop_info/ShopInformation";
 
 ReactDOM.render(
     <BrowserRouter>
@@ -12,6 +13,8 @@ ReactDOM.render(
             <Header/>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/shop_infos" component={ShopInformation} />
+
             </Switch>
             <Footer/>
         </React.StrictMode>
