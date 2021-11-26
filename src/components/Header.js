652a9ad054b376f5dcb09799081e9d4681c0fec5
mike <samkomisha@gmail.com>
2021-11-26 13:43:17 +0300

    add links and routes for Register, Login and Logout components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import {Container, Nav, NavDropdown} from "react-bootstrap";
+import {Container, Nav, NavDropdown, NavLink} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 
 export default function Header() {
@@ -8,11 +9,11 @@ export default function Header() {
         <React.Fragment>
             <Navbar bg="light" expand="lg">
               <Container>
-                <Navbar.Brand href="#home">Shop platform</Navbar.Brand>
+                <Navbar.Brand href="/">Shop platform</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                       <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -22,6 +23,10 @@ export default function Header() {
                       <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                   </Nav>
+                    <Link component={NavLink} to="/register">Register</Link>
+                    <Link component={NavLink} to="/login">Login</Link>
+                    <Link component={NavLink} to="/logout">Logout</Link>
+
                 </Navbar.Collapse>
               </Container>
             </Navbar>
