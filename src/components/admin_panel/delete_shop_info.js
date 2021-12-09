717fc3850924f12557778bd9bac6af90b8520f29
mike <samkomisha@gmail.com>
2021-12-09 17:23:01 +0300

    add component for deleting items. Ask user to confirm deleting

---
+++ src/components/admin_panel/delete_shop_info.js
@@ -0,0 +1,42 @@
+import React from "react";
+import axiosInstance from "../../axios";
+import {useHistory, useParams} from "react-router-dom";
+import {Button, Container} from "react-bootstrap";
+
+
+export default function DeleteShopInfo() {
+    const history = useHistory();
+    const {id} = useParams();
+    console.log(id);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosInstance
+            .delete(`admin/delete/shop_info/${id}/`)
+            .catch(function (error){
+                if (error.response) {
+                    console.log(error.response.data);
+                }
+            })
+            .then(function () {
+                history.push({
+                    pathname: '/admin/',
+                });
+                window.location.reload();
+            });
+    };
+
+    return (
+        <section className="vh-100">
+        <Container>
+            <div className="text-center mt-5 top-50">
+            <h2>Do you want to delete chosen shop?</h2>
+                <Button onClick={handleSubmit} className="btn-danger m-4">Delete this shop</Button>
+                <Button className="m-4" href={'/admin/'}>Go back to admin</Button>
+            </div>
+        </Container>
+        </section>
+    )
+
+
+}
